5267ed98b20b72f3ca55623ebd51f7c909febea5
joliveira2752-hash <joliveira2752@gmail.com>
2026-04-01 15:13:56 -0400

    feat: Add initial project setup
    Initializes the Git repository and sets up the remote origin for the "Controle de Frete" project. This includes basic Git commands for initializing, staging, committing, and pushing the project to GitHub.

--- src/App.jsx.tsx
+++ src/App.jsx.tsx
@@ -188,3 +188,9 @@ export default function App() {
     </div>
   )
 }Error: Error: Timeout (control socket)
+git init
+git add .
+git commit -m "primeiro commit"
+git branch -M main
+git remote add origin https://github.com/joliveira2752-hash/CONTROLE-DE-FRETE.git
+git push -u origin main
